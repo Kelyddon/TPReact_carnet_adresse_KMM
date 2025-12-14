2d0157cba58f0e8883c6e9ab36afcc31917c19c4
Kelyddon <kelyddon.diemunsch@gmail.com>
2025-12-14 18:03:00 +0100

    arret de l'ouverture de force d'une page web

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     host: '127.0.0.1',   
     port: 5173,          
     strictPort: true,    
-    open: true,
+    open: false,
     hmr: {
       host: '127.0.0.1',
       port: 5173
